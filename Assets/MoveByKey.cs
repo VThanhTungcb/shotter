b450e0f76faecfb7edbdd6a4a5036b63213727f0
VuongThanhTung <vuongthanhtung231104@gmail.com>
2025-07-11 20:47:38 +0700

    day1

---
+++ Assets/MoveByKey.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBykey : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float movingSpeed;
+
+    private void OnValidate() => characterController = GetComponent<CharacterController>();
+
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        Vector3 direction = transform.right * hInput + transform.forward * vInput;
+        characterController.SimpleMove(direction * movingSpeed);
+    }
+}
